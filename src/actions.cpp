08ed596ea80655b7735057a6a320cc7e64c58f56
Mitchell Levy <levymitchell0@gmail.com>
2023-02-09 14:00:50 -0800

    Print statements in CFG

--- src/actions.cpp
+++ src/actions.cpp
@@ -1,5 +1,7 @@
 #include "actions.h"
 
+#include <llvm/Support/Casting.h>
+#include <llvm/Support/raw_ostream.h>
 #include <sstream>
 
 #include <clang/Analysis/CFG.h>
@@ -8,33 +10,60 @@
 #include <clang/ASTMatchers/ASTMatchers.h>
 
 void VisualizeCFGAction::run(const MatchFinder::MatchResult& mr)  {
-  auto* decl = mr.Nodes.getNodeAs<FunctionDecl>("decl");
+  const FunctionDecl* decl = mr.Nodes.getNodeAs<FunctionDecl>("decl");
   Stmt* body = decl->getBody();
 
   std::unique_ptr<CFG> cfg =
       CFG::buildCFG(decl, body, mr.Context, CFG::BuildOptions());
 
+  cfg->dump(LangOptions(), true);
   this->file << "digraph g {";
 
-  std::ostringstream edges = std::ostringstream();
   for (auto itr = cfg->begin(); itr != cfg->end(); ++itr) {
     CFGBlock* curr = *itr;
 
-    curr->dump();
-
+    // Print the block itself
     int lbl = curr->getBlockID();
-    this->file << "\"" << lbl << "\" [ ];" << std::endl;
+    this->file << "\"" << lbl << "\" [";
+    if (&cfg->getEntry() != curr && &cfg->getExit() != curr && !curr->empty()) {
+      this->file << "label=<<table>";
+      std::string stmt_str;
+      llvm::raw_string_ostream raw_oss(stmt_str);
+      for (auto& elt : *curr) {
+        switch(elt.getKind()) {
+          case CFGElement::Kind::Statement: {
+            CFGStmt cfg_stmt = elt.castAs<CFGStmt>();
+            const Stmt* stmt = cfg_stmt.getStmt();
+
+            this->file << "<tr><td>";
+
+            stmt->printPretty(raw_oss, nullptr, PrintingPolicy(LangOptions()));
+            this->file << stmt_str;
+            stmt_str.clear();
+
+            this->file << "</td></tr>";
+            break;
+          }
+          default:
+            assert(false);
+        }
+      }
+      this->file << "</table>>";
+    }
+    this->file << "];" << std::endl;
+
+    // Print all the edges
     for (auto succ_itr = curr->succ_begin();
         succ_itr != curr->succ_end();
         ++succ_itr) {
-      if(succ_itr->getReachableBlock()) {
-        this->file << "\"" << lbl << "\" -> " << "\"" <<
-          succ_itr->getReachableBlock()->BlockID << "\"" << std::endl;
+      if (CFGBlock* block = succ_itr->getReachableBlock()) {
+        this->file << "\"" << lbl << "\" -> " << "\"" << block->BlockID <<
+          "\""<< std::endl;
       }
     }
   }
 
-  this->file << edges.str() << "}" << std::endl;
+  this->file << "}" << std::endl;
 }
 
 DeclarationMatcher VisualizeCFGAction::matcher() {
